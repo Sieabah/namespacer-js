ebd5e60a9e527f52a8604b415b642bd44c38a335
Christopher Sidell <csidell@asymmetrik.com>
2017-01-12 19:00:59 -0500

    Add documentation to namespacer

--- namespacer.js
+++ namespacer.js
@@ -3,13 +3,24 @@
 const path = require('path');
 
 class Namespace {
-    constructor(conf, root){
+    /**
+     * Create namespace instance with configuration
+     * @param conf {Object} Object of namespaces and folder paths
+     * @param root {string} Relative or absolute path to root
+     */
+    constructor(conf, root=null){
         if(!conf)
             throw new Error('No namespace config given');
 
         Namespace.addSpacesFromObject(conf, root);
     }
 
+    /**
+     * Resolve filepath
+     * @param req {string} Namespaced path
+     * @throws Error
+     * @returns {string}
+     */
     static resolve(req) {
         for (let space of Namespace._spaces)
             if (space.test.exec(req))
@@ -18,10 +29,24 @@ class Namespace {
         throw new Error(`'${req}' not found in namespace`);
     }
 
+    /**
+     * Ensure string ends with path separator
+     * @param str {string} Path to ensure
+     * @returns {string}
+     * @private
+     */
     static _ensureEndSlash(str){
         return str.replace(`/${path.sep}*$/`, '')+path.sep;
     }
 
+    /**
+     * Create space from name, relative path from root
+     * @param name {string} Namespace
+     * @param rel {string} Path to namespace
+     * @param root {string} Path to root
+     * @returns {{name: string, root: string, location: string, test: RegExp}}
+     * @private
+     */
     static _createSpace({ name, rel, root }){
         return {
             name,
@@ -31,6 +56,13 @@ class Namespace {
         };
     }
 
+    /**
+     * Read from configuration
+     * @param conf {Object} Namespace configuration object
+     * @param root {string} Path to root
+     * @returns {Array}
+     * @private
+     */
     static _readConf(conf, root){
         let _spaces = [];
 
@@ -45,14 +77,30 @@ class Namespace {
         return _spaces;
     }
 
+    /**
+     * Sort namespaces based on length (more specific first)
+     * @param a {string}
+     * @param b {string}
+     * @returns {number}
+     * @private
+     */
     static _spaceSorter(a,b){
         return b.name.length - a.name.length
     }
 
+    /**
+     * Sort global namespaces
+     * @private
+     */
     static _sortSpaces(){
         Namespace._spaces.sort(Namespace._spaceSorter);
     }
 
+    /**
+     * Add namespaces from object
+     * @param obj {Object} Configuration object
+     * @param root {string} Path to root
+     */
     static addSpacesFromObject(obj, root){
         let _spaces = Namespace._readConf(obj, root);
 
@@ -64,6 +112,9 @@ class Namespace {
         Namespace._sortSpaces();
     }
 
+    /**
+     * Get object of all spaces
+     */
     static getSpaces(){
         //Super efficient deep clone
         return JSON.parse(JSON.stringify(Namespace._spaces));
@@ -71,9 +122,24 @@ class Namespace {
 }
 
 if(!Namespace._spaces)
+    /**
+     * Static variable of all namespaces
+     * @type {Array}
+     * @static
+     * @private
+     */
     Namespace._spaces = [];
 
 module.exports = {
+    /**
+     * Static namespace reference
+     */
     namespace: Namespace,
+
+    /**
+     * Create namespace instance that references
+     * @param conf {Object} Configuration object to add to global namespaces
+     * @param root {string} Path to root of namespace
+     */
     instance: (conf, root) => new Namespace(conf, root)
 };
